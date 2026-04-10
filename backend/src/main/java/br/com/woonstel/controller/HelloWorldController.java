6045020a1ff1ee01ebcd8702b6d8a34e14b3989e
nelsonfornazeiro <nelsonfornazeiro@gmail.com>
2026-04-09 22:27:37 -0300

    feat: test controller to see Hello World

---
+++ backend/src/main/java/br/com/woonstel/controller/HelloWorldController.java
@@ -0,0 +1,15 @@
+package br.com.woonstel.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/testing")
+public class HelloWorldController {
+    @GetMapping
+    public String HelloWord(){
+        return "Hello World!";
+    }
+
+}
